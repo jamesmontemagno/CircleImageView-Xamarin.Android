e59ec9ae6c4f95445cf499dee12c4d638ec1719e
James Montemagno <james.montemagno@gmail.com>
2015-10-19 13:07:07 -0700

    Add NuGet

--- CircleImageView/CircleImageView.cs
+++ CircleImageView/CircleImageView.cs
@@ -14,26 +14,47 @@ using Android.Widget;
 
 namespace Refractored.Controls
 {
+    /// <summary>
+    /// Circle image view.
+    /// </summary>
     public class CircleImageView : _BaseCircleImageView
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Refractored.Controls.CircleImageView"/> class.
+        /// </summary>
+        /// <param name="context">Context.</param>
         public CircleImageView(Context context)
             : base(context)
         {
             Initialize();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Refractored.Controls.CircleImageView"/> class.
+        /// </summary>
+        /// <param name="context">Context.</param>
+        /// <param name="attrs">Attrs.</param>
         public CircleImageView(Context context, IAttributeSet attrs)
             : base(context, attrs)
         {
             Initialize();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Refractored.Controls.CircleImageView"/> class.
+        /// </summary>
+        /// <param name="context">Context.</param>
+        /// <param name="attrs">Attrs.</param>
+        /// <param name="defStyle">Def style.</param>
         public CircleImageView(Context context, IAttributeSet attrs, int defStyle)
             : base(context, attrs, defStyle)
         {
             Initialize();
         }
 
+        /// <summary>
+        /// Initialize this instance.
+        /// </summary>
         void Initialize()
         {
         }
--- CircleImageView/Properties/AssemblyInfo.cs
+++ CircleImageView/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using Android.App;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("CircleImageView")]
+[assembly: AssemblyTitle("Refractored.Controls.CircleImageView")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("jamesmontemagno")]
+[assembly: AssemblyCopyright("2015 Refractored LLC/James Montemagno")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
