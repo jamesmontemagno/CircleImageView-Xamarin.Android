633106487bfd03ce4bae41965eafdf7ceddb5fa7
James Montemagno <james.montemagno@gmail.com>
2017-11-14 00:04:21 -0500

    Update build_library_and_run_cake.ps1

--- build_library_and_run_cake.ps1
+++ build_library_and_run_cake.ps1
@@ -2,4 +2,4 @@ cd src\original_circleimageview_submodule\
 .\gradlew circleimageview:clean circleimageview:assembleRelease -x lint --stacktrace
 cp circleimageview\build\outputs\aar\circleimageview-release.aar ..\CircleImageView\Jars\
 cd ..\..\
-.\build.ps1
+.\build.ps1 -Target Default -Verbosity diagnostic
